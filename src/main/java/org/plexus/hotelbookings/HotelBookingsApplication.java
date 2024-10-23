2e57b3676d724db4fe3866d26603fd56d568164c
fernando.varelamarti <fernando.varelamarti@plexus.es>
2024-10-23 10:36:44 +0200

    add structure

---
+++ src/main/java/org/plexus/hotelbookings/HotelBookingsApplication.java
@@ -0,0 +1,13 @@
+package org.plexus.hotelbookings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelBookingsApplication.class, args);
+    }
+
+}
